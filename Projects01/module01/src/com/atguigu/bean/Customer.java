096fcc5b8716a7968616bca56a0ac64ea5c2236d
Aone <122345446@qq.com>
2019-09-29 17:38:40 +0800

    Initial commit

---
+++ Projects01/module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,22 @@
+package com.atguigu.bean;
+
+
+import java.util.ArrayList;
+
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        ArrayList array = new ArrayList();
+        ArrayList arrayList = new ArrayList();
+        System.out.println("sss");
+        System.out.println("asdf");
+
+    }
+
+
+
+    public void mothod() {
+
+    }
+}
+
---
+++ Projects01/module01/src/com/atguigu/bean/hello.java
@@ -0,0 +1,8 @@
+package com.atguigu.bean;
+
+/**
+ * @author shkstart
+ * @date 2019/9/25 - 11:13
+ */
+public class hello {
+}
